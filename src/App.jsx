eaf3ddc16a2c3cea833cf28c855094a7f70732c7
Vansh Bansal <vanshbansal@Vanshs-MacBook-Air.local>
2023-09-17 11:44:15 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import Hero from './Sections/Hero'
+import  PopularProducts  from './Sections/PopularProducts'
+import  SuperQuality  from './Sections/SuperQuality'
+import  Services  from './Sections/Services'
+import  SpecialOffer  from './Sections/SpecialOffer'
+import  CustomerReviews  from './Sections/CustomerReviews'
+import  Subscribe  from './Sections/Subscribe'
+import Footer from './Sections/Footer'
+import Nav from './Components/Nav'
+
+const App = () => (
+  <main className="relative">
+  <Nav/>
+  <section className='xl:padding-l wide:padding-r padding-b'>
+    <Hero/>
+  </section>
+  <section className="padding">
+    <PopularProducts/>
+  </section>
+  <section className="padding">
+    <SuperQuality/>
+  </section>
+  <section className="padding-x py-10">
+    <Services/>
+  </section>
+  <section className="padding">
+    <SpecialOffer/>
+  </section>
+  <section className="bg-pale-blue padding">
+    <CustomerReviews/>
+  </section>
+  <section className="padding-x sm:py-32 py-16 w-full">
+    <Subscribe/>
+  </section>
+  <section className="bg-black padding-x padding-t pb-8">
+    <Footer/>
+  </section>
+</main>
+)
+
+export default App
